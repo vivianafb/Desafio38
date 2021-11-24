ee51e9d7152d39131b8d801b2bac5780a7aef8a6
Viviana <vivianafb15@gmail.com>
2021-11-24 19:18:52 -0300

    Singleton implementado

--- src/models/productos/products.factory.js
+++ src/models/productos/products.factory.js
@@ -26,8 +26,7 @@ export class NoticiasFactoryDAO {
         logger.info('Instancia Nula. Inicializandola');
         const filePath = path.resolve(__dirname, './DAOs/products.json');
         logger.info('RETORNANDO INSTANCIA CLASE FS');
-        return this.instancia = new ProductosFSDAO(filePath);
-        
+        return this.instancia = new ProductosFSDAO(filePath);   
 
       case TipoPersistencia.MongoAtlas:
         if(!this.instancia) 
@@ -35,21 +34,17 @@ export class NoticiasFactoryDAO {
         logger.info('RETORNANDO INSTANCIA CLASE MONGO ATLAS DE PRODUCTO');
         return this.instancia = new ProductosAtlasDAO();
         
-
       case TipoPersistencia.LocalMongo:
         if(!this.instancia) 
         logger.info('Instancia Nula. Inicializandola');
         logger.info('RETORNANDO INSTANCIA CLASE MONGO LOCAL');
         return this.instancia = new ProductosAtlasDAO(true);
         
-
-
       case TipoPersistencia.MYSQL:
         if(!this.instancia) 
         logger.info('Instancia Nula. Inicializandola');
         console.log('RETORNANDO INSTANCIA CLASE MYSQL/MariDB LOCAL');
         return this.instancia = new ProductosSQLDAO();
-
 
       case TipoPersistencia.SQLITE3:
         if(!this.instancia) 
@@ -57,7 +52,6 @@ export class NoticiasFactoryDAO {
         logger.info('RETORNANDO INSTANCIA CLASE SQLITE');
         return this.instancia = new ProductosSQLITEDAO();
 
-    
       default:
         if(!this.instancia) 
         logger.info('Instancia Nula. Inicializandola');
